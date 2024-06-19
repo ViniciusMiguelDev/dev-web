5ea87360b2da791ba063b4804477512678ead45d
Vinicius Miguel <vmfasdev@gmail.com>
2024-06-19 17:32:41 -0300

    19/06/2024 (2)

---
+++ javascript/fundamentos/funcao2.js
@@ -0,0 +1,18 @@
+const imprimirSoma = function (a, b) {
+    console.log(a + b)
+};
+
+imprimirSoma(2, 3);
+
+const soma = (a, b) => {
+    return a + b
+};
+
+console.log(soma(5, 5));
+
+const subtracao = (a, b) => a - b;
+
+console.log(subtracao(8, 5));
+
+const imprimir = a => console.log(a)
+imprimir('Hello World!!!');
