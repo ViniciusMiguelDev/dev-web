742fb6504c4dc3f642349f96b6f26900672b6360
Vinicius Miguel <vmfasdev@gmail.com>
2024-06-19 12:58:17 -0300

    19/06/2024 (1)

---
+++ javascript/desafios/desafio_area.js
@@ -0,0 +1,6 @@
+// PI * raio * raio
+let raio = 10;
+let PI = Math.PI;
+
+let calcularArea = PI * raio * raio
+console.log(calcularArea)
---
+++ javascript/desafios/desafio_troca.js
@@ -0,0 +1,11 @@
+let a = 7;
+let b = 94;
+
+console.log(a);
+console.log(b);
+
+let c = a;
+a = b
+b = c
+console.log(a);
+console.log(b);
